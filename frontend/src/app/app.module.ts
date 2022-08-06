dacee12fa74e486f08542e5159f529af453d8ed4
Dongido <chima.sima@gmail.com>
2022-08-07 00:10:42 +0100

    Feat: Create menus and dashboard component
    create components for
    * dashboard
    * top navigation menu
    * sidebar navigation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TopMenuComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
